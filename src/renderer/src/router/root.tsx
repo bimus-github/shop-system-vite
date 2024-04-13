f9933d096434e763c0fa6bffd9e876154138a7a8
bimus-github <bimus2022@gmail.com>
2024-04-13 10:16:05 +0500

    fixing

--- src/renderer/src/router/root.tsx
+++ src/renderer/src/router/root.tsx
@@ -20,7 +20,9 @@ export const root = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<SideBar />}>
       <Route path={PATH_NAME.SALE} element={<Sale />} />
-      <Route path={PATH_NAME.STORAGE} element={<Storage />} />
+      <Route path={PATH_NAME.STORAGE} element={<Storage />}>
+        <Route path={':productId'} element={<PrintModal />} />
+      </Route>
       <Route path={PATH_NAME.SHOPS} element={<Shops />} />
       <Route path={PATH_NAME.PRODUCTS_IN_SHOP} element={<ProductsListInShop />}>
         <Route path={':productId'} element={<PrintModal />} />
