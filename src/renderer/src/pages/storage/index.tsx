5538b354b8d5154da42b4104ed7f54c581b3e253
bimus-github <bimus2022@gmail.com>
2024-04-13 10:16:01 +0500

    fixing

--- src/renderer/src/pages/storage/index.tsx
+++ src/renderer/src/pages/storage/index.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import { ProductsList } from '../../components/storage'
 import { Box } from '@mui/material'
 
@@ -5,6 +6,7 @@ function Storage(): JSX.Element {
   return (
     <Box sx={{ height: '80vh', width: '100%' }}>
       <ProductsList />
+      <Outlet />
     </Box>
   )
 }
