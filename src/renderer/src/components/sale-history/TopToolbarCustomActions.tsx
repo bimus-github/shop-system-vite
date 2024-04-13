cb191c5c108ebbe0acf89a216128183b65763344
bimus-github <bimus2022@gmail.com>
2024-04-13 10:15:49 +0500

    fixing

--- src/renderer/src/components/sale-history/TopToolbarCustomActions.tsx
+++ src/renderer/src/components/sale-history/TopToolbarCustomActions.tsx
@@ -1,9 +1,20 @@
-import { CopyAll, Refresh } from '@mui/icons-material'
-import { Box, Button, IconButton, Tooltip } from '@mui/material'
+import {
+  CopyAll
+  //  Refresh
+} from '@mui/icons-material'
+import {
+  Box,
+  //  Button,
+  IconButton,
+  Tooltip
+} from '@mui/material'
 import { langFormat } from '@renderer/functions/langFormat'
 import { saveAllSelected } from '@renderer/functions/saveAllSelected'
-import { useUpdateSaledProduct } from '@renderer/hooks/sale'
-import { SALE_FORM, Saled_Product_Type } from '@renderer/models/types'
+// import { useUpdateSaledProduct } from '@renderer/hooks/sale'
+import {
+  // SALE_FORM,
+  Saled_Product_Type
+} from '@renderer/models/types'
 import { MRT_TableInstance } from 'material-react-table'
 import toast from 'react-hot-toast'
 
@@ -13,7 +24,7 @@ type Props = {
 function TopToolbarCustomActions(props: Props): JSX.Element {
   const { table } = props
 
-  const { mutateAsync: updateSaledProduct } = useUpdateSaledProduct()
+  // const { mutateAsync: updateSaledProduct } = useUpdateSaledProduct()
 
   const handleSaveAll = (table: MRT_TableInstance<Saled_Product_Type>): void => {
     const selectedProducts = table.getSelectedRowModel().rows
@@ -21,69 +32,69 @@ function TopToolbarCustomActions(props: Props): JSX.Element {
     toast.success(langFormat({ uzb: 'Saqlandi', ru: 'Сохранено', en: 'Saved' }))
   }
 
-  const handleChangeSelectedRows = async (
-    table: MRT_TableInstance<Saled_Product_Type>
-  ): Promise<void> => {
-    toast((t) => (
-      <Box>
-        <div>
-          {langFormat({
-            uzb: 'Pul shaklini o`zgartirildi',
-            ru: 'Счет изменен',
-            en: 'Form of payment changed'
-          })}
-        </div>
-        <br />
-        <Button onClick={() => toast.dismiss(t.id)}>
-          {langFormat({ uzb: 'Bekor qilish', ru: 'Отмена', en: 'Cancel' })}
-        </Button>
-        <Button
-          color="error"
-          onClick={async () => {
-            table.getSelectedRowModel().rows.forEach(async (row) => {
-              const newSaleForm =
-                row.original.sale_form === SALE_FORM.CASH
-                  ? SALE_FORM.CARD
-                  : row.original.sale_form === SALE_FORM.LOAN
-                    ? SALE_FORM.CASH
-                    : SALE_FORM.LOAN
+  // const handleChangeSelectedRows = async (
+  //   table: MRT_TableInstance<Saled_Product_Type>
+  // ): Promise<void> => {
+  //   toast((t) => (
+  //     <Box>
+  //       <div>
+  //         {langFormat({
+  //           uzb: 'Pul shaklini o`zgartirildi',
+  //           ru: 'Счет изменен',
+  //           en: 'Form of payment changed'
+  //         })}
+  //       </div>
+  //       <br />
+  //       <Button onClick={() => toast.dismiss(t.id)}>
+  //         {langFormat({ uzb: 'Bekor qilish', ru: 'Отмена', en: 'Cancel' })}
+  //       </Button>
+  //       <Button
+  //         color="error"
+  //         onClick={async () => {
+  //           table.getSelectedRowModel().rows.forEach(async (row) => {
+  //             const newSaleForm =
+  //               row.original.sale_form === SALE_FORM.CASH
+  //                 ? SALE_FORM.CARD
+  //                 : row.original.sale_form === SALE_FORM.LOAN
+  //                   ? SALE_FORM.CASH
+  //                   : SALE_FORM.LOAN
 
-              const saledProduct: Saled_Product_Type = {
-                id: row.original.id,
-                name: row.original.name,
-                barcode: row.original.barcode,
-                saled_count: row.original.saled_count,
-                cost: row.original.cost,
-                saled_price: row.original.saled_price,
-                sale_form: newSaleForm,
-                buyers_name: row.original.buyers_name,
-                discount: row.original.discount,
-                saled_date: row.original.saled_date,
-                count: row.original.count,
-                price: row.original.price,
-                saledId: row.original.saledId
-              }
-              toast.dismiss(t.id)
-              const result = await toast.promise(updateSaledProduct(saledProduct), {
-                loading: langFormat({ uzb: 'Saqlanmoqda', ru: 'Сохраняется', en: 'Saving' }),
-                success: langFormat({ uzb: 'Saqlandi', ru: 'Сохранено', en: 'Saved' }),
-                error: langFormat({ uzb: 'Xatolik yuz berdi', ru: 'Ошибка', en: 'Error' })
-              })
+  //             const saledProduct: Saled_Product_Type = {
+  //               id: row.original.id,
+  //               name: row.original.name,
+  //               barcode: row.original.barcode,
+  //               saled_count: row.original.saled_count,
+  //               cost: row.original.cost,
+  //               saled_price: row.original.saled_price,
+  //               sale_form: newSaleForm,
+  //               buyers_name: row.original.buyers_name,
+  //               discount: row.original.discount,
+  //               saled_date: row.original.saled_date,
+  //               count: row.original.count,
+  //               price: row.original.price,
+  //               saledId: row.original.saledId
+  //             }
+  //             toast.dismiss(t.id)
+  //             const result = await toast.promise(updateSaledProduct(saledProduct), {
+  //               loading: langFormat({ uzb: 'Saqlanmoqda', ru: 'Сохраняется', en: 'Saving' }),
+  //               success: langFormat({ uzb: 'Saqlandi', ru: 'Сохранено', en: 'Saved' }),
+  //               error: langFormat({ uzb: 'Xatolik yuz berdi', ru: 'Ошибка', en: 'Error' })
+  //             })
 
-              if (!result) table.setRowSelection({})
-            })
-          }}
-        >
-          {langFormat({ uzb: 'Saqlash', ru: 'Сохранить', en: 'Save' })}
-        </Button>
-      </Box>
-    ))
-  }
+  //             if (!result) table.setRowSelection({})
+  //           })
+  //         }}
+  //       >
+  //         {langFormat({ uzb: 'Saqlash', ru: 'Сохранить', en: 'Save' })}
+  //       </Button>
+  //     </Box>
+  //   ))
+  // }
   return (
     <Box sx={{ display: 'flex', gap: '1rem', justifyContent: 'flex-start', width: '100%' }}>
       {table.getSelectedRowModel().rows.length > 0 && (
         <>
-          <Tooltip
+          {/* <Tooltip
             title={langFormat({
               uzb: 'Pul shaklini o`zgartirish',
               en: 'Change form of payment',
@@ -93,7 +104,7 @@ function TopToolbarCustomActions(props: Props): JSX.Element {
             <IconButton onClick={() => handleChangeSelectedRows(table)}>
               <Refresh />
             </IconButton>
-          </Tooltip>
+          </Tooltip> */}
           <Tooltip
             title={langFormat({
               uzb: 'Saqlash',
