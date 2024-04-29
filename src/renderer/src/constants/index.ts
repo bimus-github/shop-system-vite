4b842acb34615a5e3f6c30e7cbd5bdacb109194b
bimus-github <bimus2022@gmail.com>
2024-04-29 14:28:49 +0500

    fixing

--- src/renderer/src/constants/index.ts
+++ src/renderer/src/constants/index.ts
@@ -1,4 +1,4 @@
-export { default as routes } from "./routes";
-export { default as saleFormOptions } from "./saleFormOptions";
-export { default as langs } from "./langs";
-export { default as fontFamilies } from "./fontFamilies";
+export { default as routes } from './routes'
+export { default as saleFormOptions } from './saleFormOptions'
+export { default as langs } from './langs'
+export { default as fontFamilies } from './fontFamilies'
