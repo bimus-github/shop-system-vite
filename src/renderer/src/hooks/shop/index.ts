b0821ee1605e01fe22a6a94edfd127cffc8fe204
bimus-github <bimus2022@gmail.com>
2024-04-30 08:14:40 +0500

    fixing

--- src/renderer/src/hooks/shop/index.ts
+++ src/renderer/src/hooks/shop/index.ts
@@ -1,70 +1,68 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { Shop_Type } from "../../models/types";
-import {
-  createShop,
-  deleteShop,
-  getShops,
-  updateShop,
-} from "../../functions/shop";
-import { Message_Forms } from "../../models/message";
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { Shop_Type } from '../../models/types'
+import { createShop, deleteShop, getShops, updateShop } from '../../functions/shop'
+import { Message_Forms } from '../../models/message'
 
 export function useCreateShop() {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (newShop: Shop_Type) => {
-      const result = await createShop(newShop);
+      const result = await createShop(newShop)
 
       if (result === Message_Forms.SUCCESS) {
-        queryClient.setQueryData(["shops"], (prevShops: Shop_Type[]) => [
+        queryClient.setQueryData(['shops'], (prevShops: Shop_Type[]) => [
           ...(prevShops || []),
-          newShop,
-        ]);
+          newShop
+        ])
       } else {
-        return result;
+        return result
       }
-    },
-  });
+    }
+  })
 }
 
 export function useUpdateShop() {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (shop: Shop_Type) => {
-      const result = await updateShop(shop);
+      const result = await updateShop(shop)
 
       if (result === Message_Forms.SUCCESS) {
-        queryClient.setQueryData(["shops"], (prevShops: Shop_Type[]) =>
+        queryClient.setQueryData(['shops'], (prevShops: Shop_Type[]) =>
           prevShops.map((s: Shop_Type) => (s.id === shop.id ? shop : s))
-        );
+        )
       } else {
-        return result;
+        return result
       }
-    },
-  });
+    }
+  })
 }
 
 export function useDeleteShop() {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (id: string) => {
-      const result = await deleteShop(id);
+      const result = await deleteShop(id)
       if (result === Message_Forms.SUCCESS) {
-        queryClient.setQueryData(["shops"], (prevShops: Shop_Type[]) =>
+        queryClient.setQueryData(['shops'], (prevShops: Shop_Type[]) =>
           prevShops.filter((s: Shop_Type) => s.id !== id)
-        );
+        )
       } else {
-        return result;
+        return result
       }
-    },
-  });
+    }
+  })
 }
 
 export function useGetShops() {
+  const queryClient = useQueryClient()
   return useQuery<Shop_Type[]>({
-    queryKey: ["shops"],
+    queryKey: ['shops'],
     queryFn: async () => {
-      const shops: Shop_Type[] = await getShops();
-      return shops;
+      const shops: Shop_Type[] = await getShops()
+      return shops
     },
-  });
+    initialData: () => queryClient.getQueryData(['shops']) as Shop_Type[]
+  })
 }
